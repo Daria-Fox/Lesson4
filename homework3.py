a5be3eb5f04281abb8162839052ca19475bdc56f
Daria-Fox <167777808+Daria-Fox@users.noreply.github.com>
2024-04-28 19:25:27 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+my_string= input("Привет! Как тебя зовут? ")
+nice= "Рад познакомиться, " + my_string + "!"
+print(nice)
+print(len(nice))
+print(nice.upper())
+print(nice.lower())
+print(nice.replace(" ", ""))
+print(nice[0])
+print(nice[-1])
